5f14a8a79170c321439f6169d13471d1ce3880fb
mattrogrady <42446873+mattrogrady@users.noreply.github.com>
2020-04-02 15:42:33 -0700

    Added "I Am Poor" app

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Matt Ogrady on 3/31/20.
+//  Copyright © 2020 Matthew O'Grady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
