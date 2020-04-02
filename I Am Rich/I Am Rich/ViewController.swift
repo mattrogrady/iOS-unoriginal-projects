6eb765c84b8b9beed9be42c5ef16ace68edb1dfd
mattrogrady <42446873+mattrogrady@users.noreply.github.com>
2020-04-02 15:36:40 -0700

    Added "I Am Rich" app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Matt Ogrady on 2/20/20.
+//  Copyright © 2020 Matthew O'Grady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
